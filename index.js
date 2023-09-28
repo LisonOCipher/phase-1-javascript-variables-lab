9790afd0598990df66f7987ba6b09b0f6b6fc28e
LisonOCipher <lisoncipher5@gmail.com>
2023-09-28 22:53:13 +0300

    Add files via upload
    variable-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+javascript
+const fs = require('fs')
+const path = require('path')
+
+const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
+
+//company Name
+const companyName = 'Scuber';
+
+// most profitable neighborhood
+let mostProfitableNeighborhood = 'Chelsea';
+
+//  company CEO
+let companyCeo = 'Susan Smith';
+
+
+
+
